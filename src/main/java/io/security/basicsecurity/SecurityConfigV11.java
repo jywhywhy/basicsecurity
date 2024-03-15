d6cffb8985944b9ae02dd54fd4c6f53f0615c53a
song <jefkdjsf2@naver.com>
2024-03-15 17:10:46 +0900

    [섹션2] 스프링 시큐리티 주요 아키텍처 이해

--- src/main/java/io/security/basicsecurity/SecurityConfigV11.java
+++ src/main/java/io/security/basicsecurity/SecurityConfigV11.java
@@ -2,46 +2,30 @@ package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-//@EnableWebSecurity
-//@Configuration
-public class SecurityConfigV10 {
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigV11 {
 
     @Bean
-    @Order(0)
-    SecurityFilterChain filterChain1(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .securityMatcher("/admin/**")
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
-                )
-                .httpBasic(
-                        withDefaults()
-                )
-        ;
-
-        return http.build();
-    }
-
-    @Bean
-    @Order(1)
-    SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
                 )
                 .formLogin(
                         withDefaults()
                 )
         ;
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 
         return http.build();
     }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,6 +1,11 @@
 package io.security.basicsecurity;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecurityController {
 
-    @GetMapping("/")
-    public String index() {
-        return "home";
-    }
+//    @GetMapping("/")
+//    public String index() {
+//        return "home";
+//    }
 
     @GetMapping("/loginPage")
     public String loginPage() {
@@ -54,5 +59,32 @@ public class SecurityController {
         CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
         System.out.println(token.getHeaderName() + " = " + token.getToken());
         return token.getToken();
+    }
+
+    @GetMapping("/")
+    public String index(HttpSession session) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
+
+        return "home";
+    }
+
+    @GetMapping("/thread")
+    public String thread() {
+
+        new Thread(
+
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    }
+                }
+
+        ).start();
+
+        return "thread";
     }
 }
