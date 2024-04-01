bb1c687dec426a0463551326f5b8444d11470e75
song <jefkdjsf2@naver.com>
2024-04-01 16:47:26 +0900

    [섹션2] 스프링 시큐리티 주요 아키텍처 이해

--- src/main/java/io/security/basicsecurity/SecurityConfigV10.java
+++ src/main/java/io/security/basicsecurity/SecurityConfigV10.java
@@ -1,10 +1,8 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
--- src/main/java/io/security/basicsecurity/SecurityConfigV11.java
+++ src/main/java/io/security/basicsecurity/SecurityConfigV11.java
@@ -1,9 +1,7 @@
 package io.security.basicsecurity;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
